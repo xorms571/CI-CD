67fa994872cf730ad694e12b661ec2165e683aae
xorms571 <xorms6865@naver.com>
2025-02-03 15:47:01 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <SpeedInsights />
-        {/* test */}
+        {/* test 233 */}
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
